3e9fb406098a7c1aad8390bb1e7258c8610a56af
JB <11901776+ablyss74@users.noreply.github.com>
2023-09-27 12:05:54 -0400

    Create docker_startup_script.bash

---
+++ docker_startup_script.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is basically a startup script I use for starting my docker container in Clear Linux for running container apps
+# Masking the service I find helps speed up bootup procress.
+# change docker_id to match your container id. 
+
+
+systemctl unmask docker.service
+systemctl unmask docker.socket
+systemctl start docker.service
+xhost +local:${USER} && xterm -e docker run -it --privileged -v ${HOME}:/root -e JACK_NO_AUDIO_RESERVATION=1 --device /dev/snd -v /dev/shm:/dev/shm:rw --net=host -e DISPLAY=:0 docker_id
+systemctl mask docker.service
+systemctl mask docker.socket
+systemctl stop docker.service
+systemctl stop docker.socket
+systemctl stop containerd.service
